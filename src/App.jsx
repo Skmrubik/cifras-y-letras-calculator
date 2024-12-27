5ac3a784581c00a7d5c4202b21950de0b62a1a0a
JORGE\jorge <skmrubik@gmail.com>
2024-12-28 00:13:14 +0100

    búsqueda branch and bound

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+class Nodo {
+  constructor(operaciones, resultado, numerosDisponibles) {
+    this.operaciones = operaciones.slice();
+    this.resultado = resultado;
+    this.numerosDisponibles = numerosDisponibles.slice();
+  }
+
+  getOperaciones() {
+    return this.operaciones;
+  }
+
+  addOperacion(operacion) {
+    this.operaciones.push(operacion);
+  }
+
+  getResultado() {
+    return this.resultado;
+  }
+
+  setResultado(resultado) {
+    this.resultado = resultado;
+  }
+
+  getNumerosDisponibles() {
+    return this.numerosDisponibles;
+  }
+
+  getNumeroDisponible(i) {
+    return this.numerosDisponibles[i];
+  }
+  combinarNumerosDisponibles(i,j,res) {
+    this.numerosDisponibles.splice(i,1);
+    this.numerosDisponibles.splice(j-1,1);
+    this.numerosDisponibles.push(res);
+  }
+}
+
 function App() {
 
   const [casillasSeleccionadas, setCasillasSeleccionadas] = useState([]);
@@ -50,20 +87,88 @@ function App() {
     let copiaCasillasSeleccionadas = casillasSeleccionadas;
     let operaciones = [];
     console.log("hola");
-    busquedaRecursiva(copiaCasillasSeleccionadas,operaciones, 0);
+    //busquedaRecursiva(copiaCasillasSeleccionadas,operaciones, 0);
+    const startTime = performance.now();
+    branchAndBound(copiaCasillasSeleccionadas);
+    const endTime = performance.now();
+    console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
     console.log("fin");
   }
 
   function permiteDivision(a,b) {
-    if (a %b  == 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return a %b  == 0;
   }
 
   function permiteResta(a,b) {
     return a > b;
+  }
+
+  function generarHijos(nodo) {
+    let hijos = [];
+    const tamanioNumerosDisponibles = nodo.numerosDisponibles.length;
+    for (let i= 0; i<tamanioNumerosDisponibles-1; i++) {
+      for (let j= i+1; j<tamanioNumerosDisponibles; j++) {
+        const numDisponibleI = nodo.getNumeroDisponible(i);
+        const numDisponibleJ = nodo.getNumeroDisponible(j);
+        const operaciones = nodo.getOperaciones();
+        const numerosDisponibles = nodo.getNumerosDisponibles();
+        
+        const suma = numDisponibleI + numDisponibleJ;
+        let hijoSuma = new Nodo(operaciones, suma, numerosDisponibles);
+        hijoSuma.addOperacion(`${numDisponibleI}+${numDisponibleJ}=${suma}`);
+        hijoSuma.combinarNumerosDisponibles(i,j,suma);
+        hijos.push(hijoSuma);
+        if (permiteResta(numDisponibleI, numDisponibleJ)) {
+          const resta = numDisponibleI - numDisponibleJ;
+          let hijoResta = new Nodo(operaciones, resta, numerosDisponibles);
+          hijoResta.addOperacion(`${numDisponibleI}-${numDisponibleJ}=${resta}`);
+          hijoResta.combinarNumerosDisponibles(i,j,resta);
+          hijos.push(hijoResta);
+        }
+        if (permiteResta(numDisponibleJ, numDisponibleI)) {
+          const resta = numDisponibleJ - numDisponibleI;
+          let hijoResta = new Nodo(operaciones, resta, numerosDisponibles);
+          hijoResta.addOperacion(`${numDisponibleJ}-${numDisponibleI}=${resta}`);
+          hijoResta.combinarNumerosDisponibles(i,j,resta);
+          hijos.push(hijoResta);
+        }
+        const multiplicacion = numDisponibleI * numDisponibleJ;
+        let hijoMultiplicacion = new Nodo(operaciones, multiplicacion, numerosDisponibles);
+        hijoMultiplicacion.addOperacion(`${numDisponibleI}x${numDisponibleJ}=${multiplicacion}`);
+        hijoMultiplicacion.combinarNumerosDisponibles(i,j,multiplicacion);
+        hijos.push(hijoMultiplicacion);
+        if (permiteDivision(numDisponibleI, numDisponibleJ)) {
+          const division = numDisponibleI / numDisponibleJ;
+          let hijoDivision = new Nodo(operaciones, division, numerosDisponibles);
+          hijoDivision.addOperacion(`${numDisponibleI}/${numDisponibleJ}=${division}`);
+          hijoDivision.combinarNumerosDisponibles(i,j,division);
+          hijos.push(hijoDivision);
+        }
+        if (permiteDivision(numDisponibleJ, numDisponibleI)) {
+          const division = numDisponibleJ / numDisponibleI;
+          let hijoDivision = new Nodo(operaciones, division, numerosDisponibles);
+          hijoDivision.addOperacion(`${numDisponibleJ}/${numDisponibleI}=${division}`);
+          hijoDivision.combinarNumerosDisponibles(i,j,division);
+          hijos.push(hijoDivision);
+        }  
+      }
+    }
+    return hijos;
+  }
+
+  function branchAndBound(numerosDisponibles) {
+    const nodoPadre = new Nodo([], 0, numerosDisponibles);
+    let nodos = generarHijos(nodoPadre);
+    while (nodos.length> 0) {
+      const nodoHijo = nodos[0];
+      nodos.shift();
+      if (nodoHijo.getResultado() == Number(resultado)) {
+        console.log(nodoHijo.getOperaciones());
+      } else if (nodoHijo.getOperaciones().length < 5){
+        let nodosHijos = generarHijos(nodoHijo);
+        nodos = nodos.concat(nodosHijos);
+      }
+    }
   }
 
   function busquedaRecursiva(numerosDisponibles, operaciones, ultimaOperacionRes) {
